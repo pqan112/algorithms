5f2ff6144f9408ffe895fe2e0e20c2bb38e02662
AnPQ <anpham1122000@gmail.com>
2025-01-15 22:34:53 +0700

    middle-of-the-linked-list

---
+++ middle-of-the-linked-list.js
@@ -0,0 +1,48 @@
+var middleNode1 = function (head) {
+  const linkedListLength = getLinkedListLength(head);
+  const middleNode = Math.floor(linkedListLength / 2);
+
+  for (let i = 0; i <= linkedListLength; i++) {
+    if (i === middleNode) {
+      return head;
+    }
+    head = head.next;
+  }
+};
+
+function getLinkedListLength(head) {
+  let count = 0;
+  while (head !== null) {
+    count++;
+    head = head.next;
+  }
+  return count;
+}
+
+function middleNode2(head) {
+  let f = head,
+    s = head;
+
+  while (f !== null && f.next !== null) {
+    s = s.next;
+    f = f.next.next;
+  }
+
+  return s;
+}
+
+function ListNode(val, next = null) {
+  this.val = val;
+  this.next = next;
+}
+
+const head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+head.next.next.next.next = new ListNode(5);
+
+// Con trỏ nhanh di chuyển gấp đôi tốc độ của con trỏ chậm.
+// Điều này nghĩa là khi con trỏ nhanh di chuyển hết toàn bộ độ dài của linked list, con trỏ chậm sẽ di chuyển được một nửa độ dài đó.
+
+console.log(middleNode2(head));
--- test.js
+++ test.js
@@ -1,16 +1,26 @@
 for (let i = 1; i <= 5; i++) {
-  // if (i === 3) {
-  //   console.log(i);
-  //   continue; // Bỏ qua i = 3
-  // }
-  if (i === 4) {
-    console.log(i);
-    break; // Thoát khỏi vòng lặp khi i = 4
+  if (i === 3) {
+    continue; // khi i = 3 -> continue thì sẽ bỏ qua phần code còn lại của vòng lặp -> không log ra số 3
   }
-  if (i === 5) {
-    console.log(i);
-    continue; // Thoát khỏi vòng lặp khi i = 4
+  console.log(i);
+}
+// Skips the rest of the current iteration and moves to the next iteration of the loop.
+// Output: 1, 2, 4, 5
+
+// Terminates the loop or switch immediately.
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) {
+    break; // khi i = 3 -> return thì nghỉ luôn
   }
-  console.log(i); // In ra các giá trị: 1, 2
+  console.log(i);
+}
+// Output: 1, 2
+
+// Stops the function execution and optionally passes a value back to the caller.
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) {
+    return; // khi i = 3 -> return thì nghỉ luôn
+  }
+  console.log(i);
 }
 // Output: 1, 2
